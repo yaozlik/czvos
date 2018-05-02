2cf92e7d4e4b583aae3da4cae83a9374121232fe
Oscar Miguel Fernández López <31351841+yaozlik@users.noreply.github.com>
2018-05-02 17:08:51 -0500

    se cambio el reply55

--- app/src/main/java/mx/com/cocodrilozone/czvos/activity/MainActivity.java
+++ app/src/main/java/mx/com/cocodrilozone/czvos/activity/MainActivity.java
@@ -41,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void getResponse(String response) {
-        Log.d("reply44444", response + "-");
+        Log.d("reply55", response + "-");
 
     }
 
