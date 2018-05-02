92e1aa7a6afebdd45c6482a1742b7e751fdc1690
Oscar Fernandez <oscar.fernandez@globant.com>
2018-05-02 16:22:45 -0500

    add something

--- app/src/main/java/mx/com/cocodrilozone/czvos/activity/MainActivity.java
+++ app/src/main/java/mx/com/cocodrilozone/czvos/activity/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 import mx.com.cocodrilozone.czvos.R;
 import mx.com.cocodrilozone.czvos.api.LoginApi;
@@ -15,16 +16,19 @@ public class MainActivity extends AppCompatActivity {
 
     private LoginApi loginApi;
     private LoginDTO login;
-
+    private EditText userEditText;
+    private EditText passwordEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         loginApi = new LoginApi();
         login = new LoginDTO();
+        userEditText = findViewById(R.id.txtUser);
+        passwordEditText.findViewById(R.id.txtPass);
         //TODO: obtener el usuario y password de los edittext :D
-        login.setUser("user");
-        login.setPassword("password");
+        login.setUser(userEditText.getText().toString());
+        login.setPassword(passwordEditText.getText().toString());
         setContentView(R.layout.activity_main);
         findViewById(R.id.btnLogin).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,8 +40,8 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void getResponse(String response){
-        Log.d("reply", response);
+    public void getResponse(String response) {
+        Log.d("reply", response + "-");
     }
 
 }
